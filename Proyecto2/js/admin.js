be69d4bc71c6b634bd78810c3c197f25ab50c4e8
KevinChevez <kchevez@espol.edu.ec>
2022-07-12 00:34:20 -0500

    Envio cambios realizados

--- Proyecto2/js/admin.js
+++ Proyecto2/js/admin.js
@@ -1,35 +1,53 @@
 
+// Grafico 1 - Principal general
+let arrLabels = ["Sunday","M","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+let arrDatos = [15339, 21345, 18483, 24003, 23489, 24092, 12034];
+crearGrafico("myChart", "line", arrLabels, arrDatos);
 
-// Graph
-var ctx = document.getElementById("myChart");
+// Grafico 2 - Secundario
+arrLabels = ["Sabado","M","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+arrDatos = [15339, 21345, 18483, 24003, 23489, 24092, 12034];
+crearGrafico("myChart2", "bar", arrLabels, arrDatos);
 
-var myChart = new Chart(ctx, {
-  type: "line",
-  data: {
-    labels: ["Sunday","M","Tuesday","Wednesday","Thursday","Friday","Saturday"],
-    datasets: [
-      {
-        data: [15339, 21345, 18483, 24003, 23489, 24092, 12034],
-        lineTension: 0,
-        backgroundColor: "transparent",
-        borderColor: "#007bff",
-        borderWidth: 4,
-        pointBackgroundColor: "#007bff",
-      },
-    ],
-  },
-  options: {
-    scales: {
-      yAxes: [
+// Grafico 3 - Secundario
+arrLabels = ["Sabado","M","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+arrDatos = [15339, 21345, 18483, 24003, 23489, 24092, 12034];
+crearGrafico("myChart3", "line", arrLabels, arrDatos);
+
+// Grafico 4 - Secundario
+arrLabels = ["Sabado","M","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+arrDatos = [15339, 21345, 18483, 24003, 23489, 24092, 12034];
+crearGrafico("myChart4", "bar", arrLabels, arrDatos);
+
+// funcion de graficos
+async function crearGrafico(id, tipo, arrEtiquetas, arrDatos){
+  var ctx = document.getElementById(id);
+  var myChart4 = new Chart(ctx, {
+    type: tipo,
+    data: {
+      labels: arrEtiquetas,
+      datasets: [
         {
-          ticks: {
-            beginAtZero: false,
-          },
+          data: arrDatos,
+          lineTension: 0,
+          backgroundColor: "transparent",
+          borderColor: "#007bff",
+          borderWidth: 4,
+          pointBackgroundColor: "#007bff",
         },
       ],
     },
-    legend: {
-      display: false,
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: false,
+          },
+        },],
+      },
+      legend: {
+        display: false,
+      },
     },
-  },
-});
+  });
+}
