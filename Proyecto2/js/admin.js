748686a2133b6b5b5052a61d5e9ca2005a802979
KevinChevez <kchevez@espol.edu.ec>
2022-07-15 09:08:31 -0500

    Subo cambios breves de diseño y adicionalmente uso la API para precargar datos en la página inicialmente

--- Proyecto2/js/admin.js
+++ Proyecto2/js/admin.js
@@ -1,4 +1,5 @@
 const API = "https://covid19.mathdro.id/api";
+const API_Muertes_Sorted = "https://covid19.mathdro.id/api/deaths";
 
 for (const listItem of document.getElementsByClassName("list-group-item-action")) {
   listItem.addEventListener("click", clickEvent => {
@@ -13,80 +14,164 @@ window.onload = function () {
   fetch(API)
     .then(response => response.json())
     .then(data => {
-        console.log(data);
-        let linkCountries = data.countries;
-        fetch(linkCountries)
-          .then(response2 => response2.json())
-          .then(dataCountries => {
-            console.log(dataCountries);
-            let countries = dataCountries.countries;
-            for (const country of countries) {
-              let plantillaOption = `<option value="${country.iso3}">${country.name}</option>`
-              document.querySelector('div.input-group > select').innerHTML += plantillaOption;
-            }
-          })
-        ;
+      let mensajeNoChart = 
+      `<p class="text-danger justify-content-center align-self-center text-center">
+        Grafico no ha podido ser añadido debido a que no se ha encontrado datos.
+      </p>`;
+      console.log(data);
+      let linkCountries = data.countries;
+      fetch(linkCountries)
+        .then(response => response.json())
+        .then(dataCountries => {
+          console.log(dataCountries);
+          let countries = dataCountries.countries;
+          for (const country of countries) {
+            let plantillaOption = `<option value="${country.iso3}">${country.name}</option>`
+            document.querySelector('div.input-group > select').innerHTML += plantillaOption;
+          }
+        })
+      ;
+      // Grafico 1 - Principal general
+      let arrLabels = [];
+      let arrDatos = [];
+      let coloresBG = ['rgb(255, 99, 132)','rgb(54, 162, 235)','rgb(255, 205, 86)'];
+      let coloresBordes = ['rgb(255, 99, 132)','rgb(255, 159, 64)','rgb(255, 205, 86)'];
+      arrLabels.push("Confirmados");
+      arrDatos.push(data.confirmed.value);
 
+      arrLabels.push("Muertes");
+      arrDatos.push(data.deaths.value);
+
+      arrLabels.push("Recuperados");
+      arrDatos.push(data.recovered.value);
+
+      crearGrafico("myChart", "bar", "Gráfico del resumen general" ,arrLabels, arrDatos, coloresBG, "white");
+
+      // Grafico 2 - Secundario
+      // Hacemos la peticion asincronica para la data de todos los paises ordenados por muertes
+      let linkDeathsSort= data.deaths.detail;
+      fetch(linkDeathsSort)
+        .then(response => response.json())
+        .then(detailsCountries =>{
+          console.log(detailsCountries);
+          let arrLabels = [];
+          let arrDatosDeaths = [];
+          let arrDatosRecovered = [];
+          let arrDatosConfirmed = [];
+          let coloresBG = ['rgb(255, 99, 132)','rgb(54, 162, 235)','rgb(255, 205, 86)'];
+          let coloresBordes = ['rgb(255, 99, 132)','rgb(255, 159, 64)','rgb(255, 205, 86)'];
+          let counter = 0;
+          for (const country of detailsCountries) {
+            if(country.countryRegion != null){
+              arrLabels.push(country.countryRegion);
+            }
+            if(country.deaths != 0 && country.deaths != null){
+              arrDatosDeaths.push(country.deaths);
+            }
+            if(country.recovered != 0 && country.recovered != null){
+              arrDatosRecovered.push(country.recovered);
+            }
+            if(country.confirmed != 0 && country.confirmed != null){
+              arrDatosConfirmed.push(country.confirmed);
+            }
+            coloresBG.push(generateRandomColor());
+            counter++;
+            if(counter >= 11){
+              break;
+            }
+          }
+ 
+          // Grafico 2 - Secundario
+          if(arrDatosDeaths.length > 0){
+            crearGrafico("myChart2", "polarArea", "Gráfico de muertes",arrLabels, arrDatosDeaths, coloresBG, "transparent");
+          }else{
+            document.getElementById("cardChart2").innerHTML = mensajeNoChart;
+          }
+
+          // Grafico 3 - Secundario
+          if(arrDatosRecovered.length > 0){
+            crearGrafico("myChart3", "doughnut", "Gráfico de recuperados",arrLabels, arrDatosRecovered, coloresBG, "white");
+          }else{
+            document.getElementById("cardChart3").innerHTML = mensajeNoChart;
+          }
+
+          // Grafico 4 - Secundario
+          if(arrDatosConfirmed.length > 0){
+            crearGrafico("myChart4", "pie", "Gráfico de confirmados",arrLabels, arrDatosConfirmed, coloresBG, "white");
+          }else{
+            document.getElementById("cardChart4").innerHTML = mensajeNoChart;
+          }
+        })
+      ;
+      
+      document.getElementById("imagenOG").src = data.image;
     })
-  ;
+    ;
 }
 
+document.querySelector('div.input-group > select').addEventListener('change', (eventSelect) => {
+  fetch()
+});
 
-// Grafico 1 - Principal general
-let arrLabels = ["Sunday","M","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-let arrDatos = [15339, 21345, 18483, 24003, 23489, 24092, 12034];
-crearGrafico("myChart", "line", arrLabels, arrDatos, "transparent", "#007bff");
-
-// Grafico 2 - Secundario
-arrLabels = ["Sabado","M","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-arrDatos = [15339, 21345, 18483, 24003, 23489, 24092, 12034];
-crearGrafico("myChart2", "bar", arrLabels, arrDatos, "transparent", "#007bff");
-
-// Grafico 3 - Secundario
-arrLabels = ["Sabado","M","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-arrDatos = [15339, 21345, 18483, 24003, 23489, 24092, 12034];
-crearGrafico("myChart3", "line", arrLabels, arrDatos, "transparent", "#007bff");
-
-// Grafico 4 - Secundario
-arrLabels = ["Sabado","M","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-arrDatos = [15339, 21345, 18483, 24003, 23489, 24092, 12034];
-crearGrafico("myChart4", "bar", arrLabels, arrDatos, "transparent", "#007bff");
 
 // funcion de graficos
-async function crearGrafico(id, tipo, arrEtiquetas, arrDatos, bgColor, bdColor){
+async function crearGrafico(id, tipo, titulo, arrEtiquetas, arrDatos, bgColor, bdColor) {
   var ctx = document.getElementById(id);
-  var myChart4 = new Chart(ctx, {
-    type: tipo,
-    data: {
-      labels: arrEtiquetas,
-      datasets: [
-        {
-          data: arrDatos,
-          lineTension: 0,
-          backgroundColor: bgColor,
-          borderColor: bdColor,
-          borderWidth: 4,
-          pointBackgroundColor: "#007bff",
+  switch (tipo) {
+    case "bar":
+    case "line":
+      new Chart(ctx, {
+        type: tipo,
+        data: {
+          labels: arrEtiquetas,
+          datasets: [{
+              label: titulo,
+              data: arrDatos,
+              lineTension: 0,
+              backgroundColor: bgColor,
+              borderColor: bdColor,
+              borderWidth: 1,
+              pointBackgroundColor: "#007bff",
+            }
+          ],
         },
-      ],
-    },
-    options: {
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: false,
-          },
-        },],
-      },
-      legend: {
-        display: false,
-      },
-    },
-  });
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true
+            }
+          }
+        },
+      });
+      break;
+
+    case "doughnut":
+    case "polarArea":
+    case "pie":
+      new Chart(ctx, {
+        type: tipo,
+        data:  {
+          labels: arrEtiquetas,
+          datasets: [{
+            label: titulo,
+            data: arrDatos,
+            backgroundColor: bgColor,
+            borderColor: bdColor,
+            borderWidth: 2,
+            hoverOffset: 4
+          }]
+        },
+      });
+      break;
+
+    default:
+      break;
+  }
+
 }
 
 // Funcion para obtener el .json del API (data)
-async function getDataAPI (API){
+async function getDataAPI(API) {
   fetch(API)
     .then(response => response.json())
     .then(data => {
@@ -95,5 +180,15 @@ async function getDataAPI (API){
     .catch(error => {
       return error;
     })
-  ;
+    ;
 }
+
+function generateRandomColor(){
+  let maxVal = 0xFFFFFF; // 16777215
+  let randomNumber = Math.random() * maxVal; 
+  randomNumber = Math.floor(randomNumber);
+  randomNumber = randomNumber.toString(16);
+  let randColor = randomNumber.padStart(6, 0);   
+  return `#${randColor.toUpperCase()}`
+}
+console.log(generateRandomColor()); 
