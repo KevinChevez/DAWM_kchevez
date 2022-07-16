454842c13486cf639ed6f49b8e59c0751f7f3ef8
KevinChevez <kchevez@espol.edu.ec>
2022-07-16 14:03:53 -0500

    Subo cambios finales del proyecto 2

--- Proyecto2/js/admin.js
+++ Proyecto2/js/admin.js
@@ -1,5 +1,9 @@
 const API = "https://covid19.mathdro.id/api";
 const API_Muertes_Sorted = "https://covid19.mathdro.id/api/deaths";
+const mensajeNoChart =
+`<p class="text-danger justify-content-center align-self-center text-center">
+Grafico no ha podido ser añadido debido a que no se ha encontrado datos de ciudades, estados o países.
+</p>`;
 
 for (const listItem of document.getElementsByClassName("list-group-item-action")) {
   listItem.addEventListener("click", clickEvent => {
@@ -14,17 +18,11 @@ window.onload = function () {
   fetch(API)
     .then(response => response.json())
     .then(data => {
-      let mensajeNoChart =
-        `<p class="text-danger justify-content-center align-self-center text-center">
-        Grafico no ha podido ser añadido debido a que no se ha encontrado datos.
-      </p>`;
-      console.log(data);
       let rutaFlag = "https://countryflagsapi.com/png/";
       let linkCountries = data.countries;
       fetch(linkCountries)
         .then(response => response.json())
         .then(dataCountries => {
-          console.log(dataCountries);
           let countries = dataCountries.countries;
           for (const country of countries) {
             let plantillaOption = `<option value="${country.iso3}">${country.name}</option>`
@@ -36,7 +34,6 @@ window.onload = function () {
                 else throw new Error(response.status + " - El país " + country.name + " no existe en la base de datos");
               })
               .then(dataCountry => {
-                // console.log(dataCountry);
                 let numConfirmados, numMuertos, numRecuperados, total;
                 if (dataCountry.confirmed != null) {
                   numConfirmados = dataCountry.confirmed.value;
@@ -63,18 +60,18 @@ window.onload = function () {
                   .catch(error => {
                     console.log(error);
                   })
-                ;
+                  ;
               })
               .catch((error) => {
                 console.log(error);
               })
-            ;
+              ;
           }
         })
         .catch((error) => {
           console.log(error);
         })
-      ;
+        ;
       // Grafico 1 - Principal general
       let arrLabels = [];
       let arrDatos = [];
@@ -89,7 +86,7 @@ window.onload = function () {
       arrLabels.push("Recuperados");
       arrDatos.push(data.recovered.value);
 
-      crearGrafico("myChart", "bar", "Gráfico del resumen general", arrLabels, arrDatos, coloresBG, "white");
+      crearGrafico("Gráfico general", "cardChart", "myChart", "bar", "Gráfico del resumen general", arrLabels, arrDatos, coloresBG, "white");
 
       // Graficos Secundarios
       // Hacemos la peticion asincronica para la data de todos los paises ordenados por muertes
@@ -97,7 +94,6 @@ window.onload = function () {
       fetch(linkDeathsSort)
         .then(response => response.json())
         .then(detailsCountries => {
-          console.log(detailsCountries);
           let arrLabels = [];
           let arrDatosDeaths = [];
           let arrDatosRecovered = [];
@@ -127,21 +123,21 @@ window.onload = function () {
 
           // Grafico 2 - Secundario
           if (arrDatosDeaths.length > 0) {
-            crearGrafico("myChart2", "polarArea", "Gráfico de muertes", arrLabels, arrDatosDeaths, coloresBG, "transparent");
+            crearGrafico("Gráfico de muertes", "cardChart2", "myChart2", "doughnut", "Gráfico de muertes", arrLabels, arrDatosDeaths, coloresBG, "transparent");
           } else {
             document.getElementById("cardChart2").innerHTML = mensajeNoChart;
           }
 
           // Grafico 3 - Secundario
           if (arrDatosRecovered.length > 0) {
-            crearGrafico("myChart3", "doughnut", "Gráfico de recuperados", arrLabels, arrDatosRecovered, coloresBG, "white");
+            crearGrafico("Gráfico de recuperados", "cardChart3", "myChart3", "pie", "Gráfico de recuperados", arrLabels, arrDatosRecovered, coloresBG, "white");
           } else {
             document.getElementById("cardChart3").innerHTML = mensajeNoChart;
           }
 
           // Grafico 4 - Secundario
           if (arrDatosConfirmed.length > 0) {
-            crearGrafico("myChart4", "pie", "Gráfico de confirmados", arrLabels, arrDatosConfirmed, coloresBG, "white");
+            crearGrafico("Gráfico de confirmados", "cardChart4", "myChart4", "polarArea", "Gráfico de confirmados", arrLabels, arrDatosConfirmed, coloresBG, "white");
           } else {
             document.getElementById("cardChart4").innerHTML = mensajeNoChart;
           }
@@ -160,13 +156,108 @@ window.onload = function () {
 
 // Cuando se seleccione un pais
 document.querySelector('div.input-group > select').addEventListener('change', (eventSelect) => {
-  fetch()
-});
+  fetch(API+"/countries/"+eventSelect.target.value)
+    .then(async response => await response.json())
+    .then(data => {
+      // Grafico 1 - Principal general
+      let arrLabels = [];
+      let arrDatos = [];
+      let coloresBG = ['rgb(255, 99, 132)', 'rgb(54, 162, 235)', 'rgb(255, 205, 86)'];
+      // let coloresBordes = ['rgb(255, 99, 132)', 'rgb(255, 159, 64)', 'rgb(255, 205, 86)'];
+      arrLabels.push("Confirmados");
+      arrDatos.push(data.confirmed.value);
 
+      arrLabels.push("Muertes");
+      arrDatos.push(data.deaths.value);
+
+      arrLabels.push("Recuperados");
+      arrDatos.push(data.recovered.value);
+
+      crearGrafico("Gráfico general", "cardChart", "myChart", "bar", "Gráfico del resumen general", arrLabels, arrDatos, coloresBG, "white");
+
+      // Graficos Secundarios
+      // Hacemos la peticion asincronica para la data de todos los paises ordenados por muertes
+      let linkDeathsSort = data.deaths.detail;
+      fetch(linkDeathsSort)
+        .then(response => response.json())
+        .then(detailsCountries => {
+          let arrLabels = [];
+          let arrDatosDeaths = [];
+          let arrDatosRecovered = [];
+          let arrDatosConfirmed = [];
+          let coloresBG = ['rgb(255, 99, 132)', 'rgb(54, 162, 235)', 'rgb(255, 205, 86)'];
+          // let coloresBordes = ['rgb(255, 99, 132)', 'rgb(255, 159, 64)', 'rgb(255, 205, 86)'];
+          let counter = 0;
+          for (const country of detailsCountries) {
+            if (country.provinceState != null) {
+              arrLabels.push(country.provinceState);
+              if (country.deaths != 0 && country.deaths != null) {
+                arrDatosDeaths.push(country.deaths);
+              }
+              if (country.recovered != 0 && country.recovered != null) {
+                arrDatosRecovered.push(country.recovered);
+              }
+              if (country.confirmed != 0 && country.confirmed != null) {
+                arrDatosConfirmed.push(country.confirmed);
+              }
+              coloresBG.push(generateRandomColor());
+              counter++;
+              if (counter >= 21) {
+                break;
+              }
+            }
+          }
+
+          // Grafico 2 - Secundario
+          if (arrDatosDeaths.length > 0) {
+            crearGrafico("Gráfico de muertes", "cardChart2", "myChart2", "doughnut", "Gráfico de muertes", arrLabels, arrDatosDeaths, coloresBG, "transparent");
+          } else {
+            document.getElementById("cardChart2").innerHTML = mensajeNoChart;
+          }
+
+          // Grafico 3 - Secundario
+          if (arrDatosRecovered.length > 0) {
+            crearGrafico("Gráfico de recuperados", "cardChart3", "myChart3", "pie", "Gráfico de recuperados", arrLabels, arrDatosRecovered, coloresBG, "white");
+          } else {
+            document.getElementById("cardChart3").innerHTML = mensajeNoChart;
+          }
+
+          // Grafico 4 - Secundario
+          if (arrDatosConfirmed.length > 0) {
+            crearGrafico("Gráfico de confirmados", "cardChart4", "myChart4", "polarArea", "Gráfico de confirmados", arrLabels, arrDatosConfirmed, coloresBG, "white");
+          } else {
+            document.getElementById("cardChart4").innerHTML = mensajeNoChart;
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+      ;
+      fetch(API+"/countries/"+eventSelect.target.value+"/og")
+        .then(response => response.url)
+        .then(dataLink => {
+          document.getElementById("imagenOG").src = dataLink;
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+      ;
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  ;
+})
 
 // funcion de graficos
-function crearGrafico(id, tipo, titulo, arrEtiquetas, arrDatos, bgColor, bdColor) {
-  var ctx = document.getElementById(id);
+function crearGrafico(subtitle, id, idChart, tipo, titulo, arrEtiquetas, arrDatos, bgColor, bdColor) {
+  
+  document.getElementById(id).innerHTML =
+    `<canvas class="my-4 h-100" id="${idChart}"></canvas>
+    <h5 class="mb-0 text-center fs-6">${subtitle}</h5>`
+  ;
+  
+  var ctx = document.getElementById(idChart);
   switch (tipo) {
     case "bar":
     case "line":
@@ -244,49 +335,49 @@ function generateRandomColor() {
 
 function cartaBanderas(rutaIMG, paisName, numTotal, numMuertos, numConfir, numRecu) {
   return `
-    <div class="row pt-4 mx-1 my-4 rounded-3 bg-info shadow-3-strong">
-      <div class="col-xl-3 col-sm-6 col-12 mb-4 align-self-center">
-        <div class="card"> 
-          <div class="card-body">
-            <div class="d-flex justify-content-between px-md-1">
-              <div class="align-self-center">
-                <figure class="figure m-0 border border-black rounded">
-                  <img id="${paisName}" src="${rutaIMG}" class="figure-img img-fluid rounded size-flag" alt="Bandera de ${paisName}" onerror="this.src='img/imageNotAvailable.png'">
-                </figure>
-              </div>
-              <div class="text-end">
-                <h3>${paisName}</h3>
-                <p class="mb-0">Total personas afectadas: ${numTotal}</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      ${subCardPaises("fa-chart-pie text-danger", "Muertos", numMuertos)}
-      ${subCardPaises("fa-chart-line text-warning", "Confirmados", numConfir)}
-      ${subCardPaises("fa-user text-success", "Recuperados", numRecu)}
-    `
-    ;
-}
-
-function subCardPaises(iconAndColor, mensaje, valor) {
-  return `
+  <div class="row pt-4 mx-1 my-4 rounded-3 bg-info shadow-3-strong">
     <div class="col-xl-3 col-sm-6 col-12 mb-4 align-self-center">
-      <div class="card">
+      <div class="card"> 
         <div class="card-body">
           <div class="d-flex justify-content-between px-md-1">
             <div class="align-self-center">
-              <i class="fas ${iconAndColor} fa-3x"></i>
+              <figure class="figure m-0 border border-black rounded">
+                <img id="${paisName}" src="${rutaIMG}" class="figure-img img-fluid rounded size-flag" alt="Bandera de ${paisName}" onerror="this.src='img/imageNotAvailable.png'">
+              </figure>
             </div>
             <div class="text-end">
-              <h3>${valor}</h3>
-              <p class="mb-0">${mensaje}</p>
+              <h3>${paisName}</h3>
+              <p class="mb-0">Total personas afectadas: ${numTotal}</p>
             </div>
           </div>
         </div>
       </div>
     </div>
-    `
+    ${subCardPaises("fa-chart-pie text-danger", "Muertos", numMuertos)}
+    ${subCardPaises("fa-chart-line text-warning", "Confirmados", numConfir)}
+    ${subCardPaises("fa-user text-success", "Recuperados", numRecu)}
+  `
+    ;
+}
+
+function subCardPaises(iconAndColor, mensaje, valor) {
+  return `
+  <div class="col-xl-3 col-sm-6 col-12 mb-4 align-self-center">
+    <div class="card">
+      <div class="card-body">
+        <div class="d-flex justify-content-between px-md-1">
+          <div class="align-self-center">
+            <i class="fas ${iconAndColor} fa-3x"></i>
+          </div>
+          <div class="text-end">
+            <h3>${valor}</h3>
+            <p class="mb-0">${mensaje}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  `
     ;
 }
 
