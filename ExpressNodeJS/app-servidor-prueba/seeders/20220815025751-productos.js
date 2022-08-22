228651f569207f90fd011ad068eb08fe1a68d04a
KevinChevez <kchevez@espol.edu.ec>
2022-08-21 23:44:28 -0500

    Breves cambios de practica en Express

--- ExpressNodeJS/app-servidor-prueba/seeders/20220815025751-productos.js
+++ ExpressNodeJS/app-servidor-prueba/seeders/20220815025751-productos.js
@@ -4,7 +4,7 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     for (let i = 0; i <10; i++) {  
       await queryInterface.bulkInsert('Productos', [{  
-          nombre: 'Producto '+(i+1),  
+          nombre: 'Producto '+(i),  
           cantidad: 10+i,  
           createdAt: new Date(),  
           updatedAt: new Date()  
